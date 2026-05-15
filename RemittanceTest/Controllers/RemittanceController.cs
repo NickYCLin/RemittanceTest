194f7a399c9cf1c11857be60cb2d4e4630f78419
nicklin <nicklin@gis.fcu.edu.tw>
2026-05-15 15:42:22 +0800

    feat(控制器): 串接取消匯款 API，服務回傳狀態對應 200、400 與 404 回應。

--- RemittanceTest/Controllers/RemittanceController.cs
+++ RemittanceTest/Controllers/RemittanceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RemittanceTest.Models;
+using RemittanceTest.Services;
 
 namespace RemittanceTest.Controllers
 {
@@ -8,14 +9,39 @@ namespace RemittanceTest.Controllers
     public class RemittanceController : ControllerBase
     {
         // TODO: 1. 請透過建構子注入 (Constructor Injection) 引入 IRemittanceService
+        private readonly IRemittanceService _remittanceService;
+        public RemittanceController(IRemittanceService remittanceService)
+        {
+            _remittanceService = remittanceService;
+        }
 
         [HttpPost("{id}/cancel")]
         public IActionResult Cancel(int id)
         {
             // TODO: 2. 呼叫 Service 執行取消邏輯
             // TODO: 3. 根據 Service 回傳的結果，回傳相對應的 HTTP 狀態碼 (Ok / BadRequest / NotFound)
+            if (id <= 0)
+            {
+                return BadRequest(new { message = "匯款編號必須大於 0" });
+            }
 
-            return Ok();
+            var result = _remittanceService.CancelRemittance(id);
+
+            if (result.IsSuccess)
+            {
+                return Ok(new
+                {
+                    message = result.Message,
+                    status = 9
+                });
+            }
+
+            if (result.Message == "找不到匯款資料")
+            {
+                return NotFound(new { message = result.Message });
+            }
+
+            return BadRequest(new { message = result.Message });
         }
     }
 }
